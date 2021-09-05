c2548a342caaf147d4642fe1598135b9b3b3f25f
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-09-05 15:39:12 +0100

    Use async/await to handle promises

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,10 +1,57 @@
 const Event = require("../../models/Event");
 const User = require("../../models/User");
 
-const formatDate = date=> new Date(date).toISOString();
-const user = id => {
-    return User.findById(id)
-      .then(user => {
+const formatDate = date => new Date(date).toISOString();
+const user = async id => {
+  try {
+    const user = await User.findById(id);
+    return {
+      ...user._doc,
+      _id: user.id,
+      date: formatDate(user._doc.date),
+      password: null,
+      createdEvents: event.bind(this, user._doc.createdEvents)
+    };
+  } catch (err) {
+    throw err;
+  }
+};
+const event = async ids => {
+  try {
+    const events = await Event.find({ _id: { $in: ids } });
+    return events.map(event => {
+      return {
+        ...event._doc,
+        _id: event.id,
+        date: formatDate(event._doc.date),
+        creator: user.bind(this, event._doc.creator)
+      };
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = {
+  events: async () => {
+    try {
+      const events = await Event.find();
+      return events.map(event => {
+        return {
+          ...event._doc,
+          _id: event.id,
+          date: formatDate(event._doc.date),
+          creator: user.bind(this, event._doc.creator)
+        };
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+  users: async () => {
+    try {
+      const users = await User.find();
+      return users.map(user => {
         return {
           ...user._doc,
           _id: user.id,
@@ -12,126 +59,68 @@ const user = id => {
           password: null,
           createdEvents: event.bind(this, user._doc.createdEvents)
         };
-      })
-      .catch(err => {
-        throw err;
       });
-  };
-  const event = ids => {
-    return Event.find({ _id: { $in: ids } })
-      .then(events => {
-        return events.map(event => {
-          return {
-            ...event._doc,
-            _id: event.id,
-            date: formatDate(event._doc.date),
-            creator: user.bind(this, event._doc.creator)
-          };
-        });
-      })
-      .catch(err => {
-        throw err;
+    } catch (err) {
+      throw err;
+    }
+  },
+  user: async args => {
+    try {
+      const { _id } = args;
+      const user = await User.find({ _id });
+      return user;
+    } catch (err) {
+      throw err;
+    }
+  },
+  createEvent: async args => {
+    try {
+      const {
+        eventInput: { title, description, price, date }
+      } = args;
+      const event = new Event({
+        title,
+        description,
+        price: +price,
+        date: new Date(),
+        creator: "613386ffd5a87e1938ab66a6"
       });
-  };
-
-  module.exports = {
-    events: () => {
-        return Event.find()
-          .then(events => {
-            return events.map(event => {
-              return {
-                ...event._doc,
-                _id: event.id,
-                date: formatDate(event._doc.date),
-                creator: user.bind(this, event._doc.creator)
-              };
-            });
-          })
-          .catch(err => {
-            throw err;
-          });
-      },
-      users: () => {
-        return User.find()
-          .then(users => {
-            return users.map(user => {
-              return {
-                ...user._doc,
-                _id: user.id,
-                date: formatDate(user._doc.date),
-                password: null,
-                createdEvents: event.bind(this, user._doc.createdEvents)
-              };
-            });
-          })
-          .catch(err => {
-            throw err;
-          });
-      },
-      user: args => {
-        const { _id } = args;
-        return User.find({ _id })
-          .then(user => {
-            return user;
-          })
-          .catch(err => {
-            throw err;
-          });
-      },
-      createEvent: args => {
-        const {
-          eventInput: { title, description, price, date }
-        } = args;
-        const event = new Event({
-          title,
-          description,
-          price: +price,
-          date: new Date(),
-          creator: "613386ffd5a87e1938ab66a6"
-        });
-        let createdEvent;
-        return event
-          .save()
-          .then(result => {
-            createdEvent = { ...result._doc, _id:result.id, creator: user.bind(this, result._doc.creator) };
-            return User.findById("613386ffd5a87e1938ab66a6");
-          })
-          .then(user => {
-            if (!user) {
-              throw new Error("User doesn't exist!");
-            }
-            user.createdEvents.push(createdEvent);
-            return user.save();
-          })
-          .then(res => {
-            return createdEvent;
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      },
-      createUser: args => {
-        const { name, email, password } = args.userInput;
-        // check if user already exist
-        return User.findOne({ email })
-          .then(result => {
-            if (result) {
-              throw new Error("User already exists!");
-            }
-            const user = new User({
-              name,
-              email,
-              password,
-              date: new Date()
-            });
-            return user.save();
-          })
-          .then(result => {
-            delete result._doc.password;
-            return { ...result._doc };
-          })
-          .catch(err => {
-            throw err;
-          });
+      let createdEvent;
+      const result = await event.save();
+      createdEvent = {
+        ...result._doc,
+        _id: result.id,
+        creator: user.bind(this, result._doc.creator)
+      };
+      const user = await User.findById("613386ffd5a87e1938ab66a6");
+      if (!user) {
+        throw new Error("User doesn't exist!");
       }
+      await user.createdEvents.push(createdEvent);
+      await user.save();
+      return createdEvent;
+    } catch (error) {
+      throw error;
+    }
+  },
+  createUser: async args => {
+    try {
+      const { name, email, password } = args.userInput;
+      // check if user already exist
+      const userExit = await User.findOne({ email });
+      if (userExit) {
+        throw new Error("User already exists!");
+      }
+      const user = new User({
+        name,
+        email,
+        password,
+        date: new Date()
+      });
+      const result = await user.save();
+      return { ...result._doc, _id: result.id, password: null };
+    } catch (error) {
+      throw error;
+    }
   }
+};
--- src/graphql/schemas/index.js
+++ src/graphql/schemas/index.js
@@ -1,47 +1,46 @@
 const { buildSchema } = require("graphql");
 
 module.exports = buildSchema(`
-
-type Event {
+    type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+        creator: User!
+    }
+    input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
+    type User {
     _id: ID!
-    title: String!
-    description: String!
-    price: Float!
+    name: String!
+    email: String!
+    password: String
     date: String!
-    creator: User!
-}
-input EventInput {
-    title: String!
-    description: String!
-    price: Float!
-    date: String!
-}
-type User {
-  _id: ID!
-  name: String!
-  email: String!
-  password: String
-  date: String!
-  createdEvents: [Event!]
-}
+    createdEvents: [Event!]
+    }
 
-input UserInput {
-  name: String!
-  email: String!
-  password: String!
-}
+    input UserInput {
+    name: String!
+    email: String!
+    password: String!
+    }
 
-type RootQuery {
-    events: [Event!]!
-    users: [User!]!
-    user(_id: ID!): User!
-}
-type RootMutation {
-    createEvent(eventInput: EventInput): Event
-    createUser(userInput: UserInput): User
-}
-schema {
-    query: RootQuery
-    mutation: RootMutation
-}
+    type RootQuery {
+        events: [Event!]!
+        users: [User!]!
+        user(_id: ID!): User!
+    }
+    type RootMutation {
+        createEvent(eventInput: EventInput): Event
+        createUser(userInput: UserInput): User
+    }
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
 `);
